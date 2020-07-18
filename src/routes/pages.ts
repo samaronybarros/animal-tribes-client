8fd1050a88e3434773af5e7e4308756d69728023
Sam Barros <samarony.barros@gmail.com>
2020-07-18 17:27:28 +0200

    feat: routes pages

---
+++ src/routes/pages.ts
@@ -0,0 +1,7 @@
+export const HOME = '/'
+export const SIGNUP = '/signup'
+export const LOGIN = '/login'
+export const TRIBE = '/tribe'
+export const BATTLE = '/battle'
+export const PROFILE = '/profile'
+export const ABOUT = '/about'
