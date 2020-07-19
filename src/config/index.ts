a61e009cbb8c4773220a8f3debef772e79c01e32
Sam Barros <samarony.barros@gmail.com>
2020-07-19 16:37:17 +0200

    feat: tmp url

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,13 +1,12 @@
 type Config = {
-    baseUrl: string
-  }
-  
-  const dev: string = 'http://localhost:4000/graphql'
-  const prod: string = '<not yet>'
-  
-  const config: Config = {
-    baseUrl: process.env.NODE_ENV === 'development' ? dev : prod,
-  }
-  
-  export default config
-  
+  baseUrl: string
+}
+
+const dev: string = 'http://localhost:4000/graphql'
+const prod: string = 'https://animal-tribes-test-server.herokuapp.com/graphql'
+
+const config: Config = {
+  baseUrl: process.env.NODE_ENV === 'development' ? dev : prod,
+}
+
+export default config
