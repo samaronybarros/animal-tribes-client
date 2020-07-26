ee8c62d1c460e4484c4a46774bb05a5d2f9fdf3f
Sam Barros <samarony.barros@gmail.com>
2020-07-26 12:06:28 +0200

    fix: server

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,7 @@ type Config = {
 }
 
 const dev: string = 'http://localhost:4000/graphql'
-const prod: string = 'https://animal-tribes-test-server.herokuapp.com/graphql'
+const prod: string = 'https://animal-tribes-server.herokuapp.com/graphql'
 
 const config: Config = {
   baseUrl: process.env.NODE_ENV === 'development' ? dev : prod,
