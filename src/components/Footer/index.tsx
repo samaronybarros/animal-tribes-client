34994cf0a167b33af0e6b652fc1d01df79d506a4
Sam Barros <samarony.barros@gmail.com>
2020-08-29 13:02:29 +0200

    fix: arr

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -18,7 +18,7 @@ export const Footer = (): JSX.Element => {
   return (
     <StyledFooter
       className="example"
-      copyrights={`Animal Tribes ${new Date().getFullYear()} © All Rights Reserved`}
+      copyrights={`Animal Tribes ${new Date().getFullYear()} © All Rights Reserved - Image rights belong to Pinterest and Google`}
       links={
         <ul>
           <Link description="Sam Barros" to="http://sambarros.com/" />
@@ -38,7 +38,15 @@ export const Footer = (): JSX.Element => {
       <h5 className="white-text">Animal Tribes</h5>
       <p className="grey-text text-lighten-4">
         This game was created for didactic reasons. You can find how to create
-        this game right here.
+        this game right{' '}
+        <a
+          href="https://itnext.io/animal-tribes-how-to-create-your-first-full-stack-typescript-graphql-application-76786e5520ed"
+          style={{ color: 'white', fontWeight: 'bold' }}
+          target="_blank"
+        >
+          here
+        </a>
+        .
       </p>
     </StyledFooter>
   )
